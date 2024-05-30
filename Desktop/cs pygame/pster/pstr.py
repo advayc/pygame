93029b0d67f880c8a9b08724f37f9c2f6a6a7be5
Advay Chandorkar <advay1612@icloud.com>
2024-05-30 09:36:11 -0400

    bird poster

--- Desktop/cs pygame/pster/pstr.py
+++ Desktop/cs pygame/pster/pstr.py
@@ -3,33 +3,45 @@ from pygame.locals import QUIT
 
 def gotoPage3(screen, background):
     
-    background.fill((255, 255, 255))
-    
-    # Rect(left, top, width, height) and pygame.draw.rect(surface, color, rect, width)
-    pygame.draw.rect(background, (255, 255, 255), [400, 0, 100, 100], 0)
+    background.fill((5, 20, 23))
+    image=pygame.image.load('dbird.jpg')
+    img = pygame.transform.scale(image, (366.25,244.125))
+    img = img.convert()
+
+    #                   what        color    where on bg    width
+    pygame.draw.rect(background, (179, 237, 252),[90,350,100,35], 50)
+    pygame.draw.rect(background, (179, 237, 252),[270,350,100,35], 50)
+    pygame.draw.rect(background, (179, 237, 252),[440,350,100,35], 50)
     screen.blit(background, (0, 0))
+    screen.blit(img, (130, 25))
     
 def gotoPage2(screen, background):
     
-    background.fill((0, 0, 0))
-    newChess = newChess.convert()
-    
-    # Rect(left, top, width, height) and pygame.draw.rect(surface, color, rect, width)
-    
-    pygame.draw.rect(background, (255, 255, 255), [600, 0, 60, 80], 4) # first light blue
-    
-    pygame.draw.rect(background, (255, 255, 255), [600, 400, 60, 80], 4) # first light blue
-    screen.blit(background, (0, 0))
+    background.fill((6, 23, 5))
+    image=pygame.image.load('eagle.webp')
+    img = pygame.transform.scale(image, (222.25,222.25))
+    img = img.convert()
 
+    #                   what        color    where on bg    width
+    pygame.draw.rect(background, (183, 252, 179),[90,350,100,35], 50)
+    pygame.draw.rect(background, (183, 252, 179),[270,350,100,35], 50)
+    pygame.draw.rect(background, (183, 252, 179),[440,350,100,35], 50)
+    screen.blit(background, (0, 0))
+    screen.blit(img, (190, 25))
+ 
 def gotoPage1(screen, background):
     
-    background.fill((255, 0, 0))
-    image=
-    #                   where        color    where on bg    width
-    pygame.draw.rect(background, (255,255,255),[90,350,100,35], 50)
-    pygame.draw.rect(background, (255,255,255),[270,350,100,35], 50)
-    pygame.draw.rect(background, (255,255,255),[440,350,100,35], 50)
+    background.fill((23, 5, 5))
+    image=pygame.image.load('bird.jpg')
+    img=pygame.transform.scale(image, (320, 213))
+    img = img.convert()
+
+    #                   what        color    where on bg    width
+    pygame.draw.rect(background, (255, 163, 163),[90,350,100,35], 50)
+    pygame.draw.rect(background, (255, 163, 163),[270,350,100,35], 50)
+    pygame.draw.rect(background, (255, 163, 163),[440,350,100,35], 50)
     screen.blit(background, (0, 0))
+    screen.blit(img, (150, 25))
 
 
 pygame.init()
@@ -58,14 +70,20 @@ while True:
             pygame.quit()
             sys.exit()
         
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN: #90,350,100,35
             x = event.pos[0]
             y = event.pos[1]
-            if x >=400 and x <= 500 and y >=0 and y <=100 and pageNumber == 1:
-                pageNumber = 2
-            elif x >=400 and x <= 500 and y >=0 and y <=100 and pageNumber == 2:
+            if x >=90 and x <= 180 and y >=350 and y <=380 and pageNumber == 1: # button one 
+                print('You are already on Page 1!!')
+            elif x >=270 and x <= 370 and y >=350 and y <=380 and pageNumber == 2: # button two
+                print('You are already on page 2!!')
+            elif x >=450 and x <=550 and y >=350 and y <=380 and pageNumber == 3: # button three
+                print("You are already on page 3!!")
+            elif x >=90 and x <= 180 and y >=350 and y <=380 and pageNumber == 2 or pageNumber == 3: # button one 
                 pageNumber = 1
-            elif x >=600 and x <=640 and y >=400 and y <=480 and pageNumber == 2:
-                print("This is the bottom box")
-                
+            elif x >=270 and x <= 370 and y >=350 and y <=380 and pageNumber == 1 or pageNumber == 3: # button two
+                pageNumber = 2
+            elif x >=450 and x <=550 and y >=350 and y <=380 and pageNumber == 1 or pageNumber == 2: # button three
+                pageNumber = 3
+    
     pygame.display.update() 
