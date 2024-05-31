0a81387f41eed03e808dada89ca0975bd2010784
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-05-31 00:28:55 -0400

    namefield

--- Name Field - 5/namefield.py
+++ Name Field - 5/namefield.py
@@ -16,7 +16,8 @@ field_surf.fill((255,255,255))
 my_font = pygame.font.SysFont("helvetica", 20)
 field_value = ""
 field = my_font.render(field_value, True, (0,0,0))
-
+c=''
+text = my_font.render(c, True, (255, 0, 0))
 clock = pygame.time.Clock()
 while True:
     clock.tick(30)    
@@ -26,18 +27,19 @@ while True:
             sys.exit() 
         elif ev.type == pygame.KEYDOWN:
             if ev.key == pygame.K_RETURN:    #when enter is pressed, your text is 
-              print (field_value)            #outputted on the console
               f = field_value.split()
-              i = (field_value[0],f[1][0])
+              initals = (field_value[0], f[1][0])
+              c='.'.join(initals)
+              text = my_font.render(c, True, (255, 0, 0))
             elif ev.key == pygame.K_BACKSPACE and len(field_value) > 0:
                 field_value = field_value[:-1] #cut off last character
             elif (ev.unicode.isalnum() or ev.key==pygame.K_SPACE) and len(field_value) < 20:
                 field_value += ev.unicode #adds character value of key
             field = my_font.render(field_value, True, (0,0,0))
-            text=my_font.render(i, True, (0,0,0))
     
     screen.blit(background, (0, 0))
     screen.blit(field_surf, (50, 25))
-    screen.blit(field, (60, 40))
-    screen.blit(text, (100,80))
+    screen.blit(field, (60, 40)) 
+    screen.blit(text, (200, 170))
+
     pygame.display.flip()
