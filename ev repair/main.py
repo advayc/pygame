821fbae2b76d6d8dd78805729af2498e4f960bc1
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-06-09 21:15:52 -0400

    c

--- ev repair/main.py
+++ ev repair/main.py
@@ -43,6 +43,49 @@ def welcome():
     screen.fill((0, 0, 0))
     pygame.display.flip()
 
+def loading():
+    global car_speed, car2_speed, text_alpha, text_direction, crash_sound_played, transition_complete
+        # im sorry ms quan i know we didn't learn this but it is so helpful here to organize our code pls dont fail us 
+    
+    text_rect.y += text_speed * text_direction
+
+    # Move the cars based off car speed 
+    car_rect.x += car_speed
+    car2_rect.x -= car2_speed
+
+    # Change direction if the text hits the top or bottom of the screen
+    if text_rect.top <= 100 or text_rect.bottom >= 250:
+        text_direction *= -1
+
+    # Check for collision between the cars
+    if car_rect.colliderect(car2_rect):
+        # Stop the cars
+        if text_alpha > 0:
+            text_alpha -= 5  # Decrease the alpha value to fade out the text
+        else:
+            text_alpha = 0
+            transition_complete = True
+        if not crash_sound_played:
+            crash.play()
+            crash_sound_played = True
+
+    # Update text surface with new alpha value
+    text.set_alpha(text_alpha)
+
+    # Draw everything
+    screen.fill((255, 255, 255))
+    screen.blit(text, text_rect)
+    screen.blit(car_image, car_rect)
+    screen.blit(car2_image, car2_rect)
+    pygame.display.flip()
+
+    if car_rect.right > 415:
+        car_rect.x -= 2
+        pygame.display.flip()
+    if car2_rect.left < 400:
+        car2_rect.x += 2
+        pygame.display.flip()
+
 while True:
     clock.tick(60)
     for event in pygame.event.get():
@@ -52,39 +95,6 @@ while True:
     
     if not transition_complete:
         # Move the text
-        text_rect.y += text_speed * text_direction
-
-        # Move the cars
-        car_rect.x += car_speed
-        car2_rect.x -= car2_speed
-
-        # Change direction if the text hits the top or bottom of the screen
-        if text_rect.top <= 100 or text_rect.bottom >= 250:
-            text_direction *= -1
-
-        # Check for collision between the cars
-        if car_rect.colliderect(car2_rect):
-            # Stop the cars
-            car_speed = 0
-            car2_speed = 0
-            
-            if text_alpha > 0:
-                text_alpha -= 5  # Decrease the alpha value to fade out the text
-            else:
-                text_alpha = 0
-                transition_complete = True
-            if not crash_sound_played:
-                crash.play()
-                crash_sound_played = True
-
-        # Update text surface with new alpha value
-        text.set_alpha(text_alpha)
-
-        # Draw everything
-        screen.fill((255, 255, 255))
-        screen.blit(text, text_rect)
-        screen.blit(car_image, car_rect)
-        screen.blit(car2_image, car2_rect)
-        pygame.display.flip()
+        loading()
     else:
         welcome()  # Transition to the next screen
