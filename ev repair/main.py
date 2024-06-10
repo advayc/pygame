0f7fa45898c860c0c2b1117b7004d106f7751753
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-06-09 22:58:59 -0400

    uh

--- ev repair/main.py
+++ ev repair/main.py
@@ -5,7 +5,7 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("EV Repair - Made by Advay and Sohun")
+pygame.display.set_caption("Animation Beginning")
 
 # Load images
 car = pygame.image.load("car.png")
@@ -25,28 +25,46 @@ car2_speed = 2
 clock = pygame.time.Clock()
 
 # Text setup
-my_font = pygame.font.SysFont('Impact', 80)
+my_font = pygame.font.Font('FrancoisOne-Regular.ttf', 80)
 text = my_font.render('Loading...', True, (255, 0, 0))
 text_rect = text.get_rect(center=(400, 150))
 text_alpha = 255  # Initial alpha value
 text_speed = 1
 text_direction = 1
 
+# Background Sound
+bg_sound = pygame.mixer.Sound("sound.mp3")
+bg_sound.play(-1)  # Play the background sound in a loop
 # Sound setup
 crash = pygame.mixer.Sound("crash.mp3")
 crash_sound_played = False
-
-# State flag
 transition_complete = False
 
-def welcome(): # advay
-    screen.fill((0, 0, 0))
+def welcome(): # sohun + advay changed font + made outline
+    # Text setup
+    my_font = pygame.font.Font('FrancoisOne-Regular.ttf', 125)
+    text = my_font.render('EV REPAIR', True, (255, 255, 255))
+    text_rect = text.get_rect(center=(400, 150))
+    text_speed = 1
+    text_direction = 1
+    text_rect.y += text_speed * text_direction    
+    if text_rect.top <= 100 or text_rect.bottom >= 250:
+        text_direction *= -1
+    screen.fill((40,40,40))
+
+    # Render the outline
+    for x in range(-3, 4):
+        for y in range(-3, 4):
+            outline = my_font.render('EV REPAIR', True, (0,0,0))
+            screen.blit(outline, (text_rect.x + x, text_rect.y + y))
+
+    # Render the text
+    screen.blit(text, text_rect)
     pygame.display.flip()
 
-
-def loading(): # written by sohun
+def loading(): # sohun + advay
     global car_speed, car2_speed, text_alpha, text_direction, crash_sound_played, transition_complete
-        # im sorry ms quan i know we didn't learn this but it is so helpful here to organize our code pls dont fail us (advay)
+        # im sorry ms quan i know we didn't learn this but it is so helpful here to organize our code pls dont fail us 
     
     text_rect.y += text_speed * text_direction
 
@@ -69,12 +87,13 @@ def loading(): # written by sohun
         if not crash_sound_played:
             crash.play()
             crash_sound_played = True
+            bg_sound.stop()  # Stop the background sound when the crash sound is played
 
     # Update text surface with new alpha value
     text.set_alpha(text_alpha)
 
     # Draw everything
-    screen.fill((255, 255, 255))
+    screen.fill((40,40,40))
     screen.blit(text, text_rect)
     screen.blit(car_image, car_rect)
     screen.blit(car2_image, car2_rect)
